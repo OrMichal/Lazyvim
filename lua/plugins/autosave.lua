12aa213bdf222196d94655f09fb72de568fb975a
M2i0CH0a6L <misaorsah5@gmail.com>
2025-06-17 09:26:12 +0200

    ...

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,19 +1,16 @@
--- Autosave při ztrátě fokusu okna nebo po pauze 1000ms (1s)
-vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "InsertLeave" }, {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modified then
-      vim.cmd("silent! write")
-    end
+return {
+  "Pocco81/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  config = function()
+    require("auto-save").setup({
+      enabled = true,
+      execution_message = {
+        message = function()
+          return ("Saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+    })
   end,
-})
-
--- Volitelně můžeš přidat i ukládání po nečinnosti
-vim.api.nvim_create_autocmd("CursorHoldI", {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modified then
-      vim.cmd("silent! write")
-    end
-  end,
-})
+}
